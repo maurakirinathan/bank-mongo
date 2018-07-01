1ed2395a6e14aeaab09d7f3396fef38a529bda03
maurakirinathan <maurakiri@gmail.com>
2018-07-01 18:05:34 +0530

    changes

--- config.js
+++ config.js
@@ -1,15 +1,3 @@
-/**
- * URL connection format
- * mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
- * 
- * In below connection URL format
- * localhost:27017 = server:port, default port is 27017, port value is optional
- * test = our database name
- * 
- * See more: https://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html
- */
-
-
 var host ='localhost';
 var port ='27017';
 var db ='senz';
